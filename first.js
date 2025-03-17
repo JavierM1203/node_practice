4b8f573216ce082db22654db6e136b426955e147
Javier Moreno <javier.moreno@correo.ucu.edu.uy>
2025-03-17 20:24:12 -0300

    a

--- first.js
+++ first.js
@@ -1,7 +1,10 @@
-function main() {
+async function main() {
     console.log("Hola mundo");
+    const req = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    if(req.ok) {
+        const data = await req.json();
+        console.log(data);
+    }
 }
 
 main();
-
-//async fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
